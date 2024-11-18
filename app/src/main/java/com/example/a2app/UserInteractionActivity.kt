e90e53b6cb401ec384cdffa1725b562a0265f0a3
sudip <chsudip30@gmail.com>
2024-11-18 23:51:02 +0545

    Second

---
+++ app/src/main/java/com/example/a2app/UserInteractionActivity.kt
@@ -0,0 +1,75 @@
+package com.example.a2app
+
+import android.content.DialogInterface
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+
+class UserInteractionActivity : AppCompatActivity() {
+
+    lateinit var buttonAlert: Button
+    lateinit var buttonSnack: Button
+    lateinit var buttonToast: Button
+    lateinit var main: ConstraintLayout
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_user_interaction)
+        buttonAlert = findViewById(R.id.btnAlert)
+        buttonSnack = findViewById(R.id.btnSnack)
+        buttonToast = findViewById(R.id.btnToast)
+        main = findViewById(R.id.main)
+
+        buttonToast.setOnClickListener{
+            Toast.makeText(
+                this@UserInteractionActivity,
+                "Invalid login details",
+                Toast.LENGTH_LONG
+
+            ).show()
+        }
+
+        buttonSnack.setOnClickListener{
+            Snackbar.make(main,
+                "No Internet  Connection",
+                Snackbar.LENGTH_LONG).setAction("Retry",{
+
+            }).show()
+
+        }
+
+        buttonAlert.setOnClickListener {
+            val alert = AlertDialog.Builder(this@UserInteractionActivity)
+
+            alert.setTitle("Conform")
+                .setMessage("Are you sure?")
+                .setIcon(R.drawable.sony)
+                .setPositiveButton("Yes",DialogInterface.OnClickListener { dialogInterface, i ->
+                    //logout ko logic
+                })
+
+
+
+                .setNegativeButton("No",DialogInterface.OnClickListener { dialogInterface, i -> dialogInterface.dismiss() })
+            alert.create().show()
+        }
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
